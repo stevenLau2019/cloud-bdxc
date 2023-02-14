185fc8acba1bb3f2f86e012ddc68c54efe881823
stevenLau2019 <1490200861@qq.com>
2023-02-14 13:53:49 +0800

    提交

--- user-service/src/main/java/com/bdxc/user/web/UserController.java
+++ user-service/src/main/java/com/bdxc/user/web/UserController.java
@@ -3,10 +3,14 @@ import com.bdxc.user.pojo.User;
 import com.bdxc.user.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Slf4j
 @RestController
@@ -15,6 +19,12 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+//    @Value("${pattern.dateformat}")
+//    private String dateformat;
+
+     @Value("${pattern.dateformat}")
+     private String dateformat;
 
     /**
      * 路径： /user/110
@@ -25,5 +35,11 @@ public class UserController {
     @GetMapping("/{id}")
     public User queryById(@PathVariable("id") Long id) {
         return userService.queryById(id);
+    }
+
+    @GetMapping("/now")
+    public String now(){
+        System.out.println(dateformat);
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern(dateformat));
     }
 }
